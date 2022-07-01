b6a5760f74f199bcbacca166135105f96d6bd8f9
ChengxiZou <czou2@ncsu.edu>
2022-06-30 21:27:24 -0400

    readme.r

---
+++ README.R
@@ -0,0 +1,6 @@
+library(rmarkdown)
+render("D:/5th semester/ST558/Rrepo/ChengxiZou.github.io/_Rmd/2022-06-29-Third Blog Post.rmd",output_format = "github_document",
+        output_dir = "D:/5th semester/ST558/Rrepo/ChengxiZou.github.io/_posts",
+        output_options = list(html_preview = FALSE),
+        output_file = "2022-06-29-Third-Blog-Post.md")
+
